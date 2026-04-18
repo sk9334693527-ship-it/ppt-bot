d19ea485d87bb6abec66dda609c36ddd8596e370
sk9334693527-ship-it <sk9334693527@gmail.com>
2026-04-18 21:40:57 +0530

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Hello Bot Running")
+import os
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("✅ Bot working hai!")
+
+def main():
+    app = Application.builder().token(BOT_TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+
+    print("Bot running...")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
